5b1bfd1b62909e3535e31474e1f6782678df5f11
Ali-95705Al <120209968+Ali-95705Al@users.noreply.github.com>
2023-03-13 19:55:14 +0300

    Task_1       Program

--- Program.cs
+++ Program.cs
@@ -1,36 +1,23 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
-//Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные
+ //натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
 
-Console.Write("Введите число M: ");
+
+Console.Write("Введите значение M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число N: ");
+Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
-AkkermanFunction(m,n);
-
-
-// вызов функции Аккермана
-void AkkermanFunction(int m, int n)
+void che(int m, int n)
 {
-    Console.Write(Akkerman(m, n)); 
-}
-
-// функция Аккермана
-int Akkerman(int m, int n)
+if (m > n)
+return;
+if (m % 2 == 0)
 {
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (n == 0 && m > 0)
-    {
-        return Akkerman(m - 1, 1);
-    }
-    else
-    {
-        return (Akkerman(m - 1, Akkerman(m, n - 1)));
-    }
+Console.Write($"{m}, ");
 }
+che(m+1,n);
+
+}
+che(m,n);
